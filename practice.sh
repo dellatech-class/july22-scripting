c0982966587e3c9182cb5c18d4bfe5ee8cccc7ca
John D <dzagbojohn@gmail.com>
2023-02-03 11:26:21 -0500

    added practice

---
+++ practice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Demo Comment
+###### 2023 is my year of UNLIMITED POSIBILITIES#####
+echo
+##########################UNDENIABLE PROMOTIONS######
+echo
+##########################UNEXPLIANABLE PROGRESS#####    
+echo
+whoami
+echo
+pwd
+echo
+
